44982d5fd7c6190c60bf55517557d474ea2a4a1f
Terje Sannum <terje.sannum@nav.no>
2018-08-20 10:08:55 +0200

    key bindings for next-history

--- emacs/init.el
+++ emacs/init.el
@@ -146,6 +146,7 @@
 
 (require 'shell)
 (define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
+(define-key shell-mode-map (kbd "C-n") 'comint-next-input)
 
 (setenv "PAGER" "cat")
 (require 'tramp)
