d4e87a70817969bb65b16ddaf01dd4fcf99ae17d
Terje Sannum <terje.sannum@nav.no>
2019-02-03 12:33:49 +0100

    moved shell setup to separate file

--- emacs/init.el
+++ emacs/init.el
@@ -4,12 +4,16 @@
   (tool-bar-mode -1)
   (load-file (concat ts-emacs-dir "/github.com/cyberpunk-theme.el/cyberpunk-theme.el"))
   )
-
-(defvar local-domains nil "List of local domains")
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq save-abbrevs 'silently)
 
 (let ((local-settings (concat ts-emacs-dir "/local.el")))
   (when (file-exists-p local-settings)
     (load-library local-settings)))
+
+(require 'ts-shell)
 
 (add-to-list 'load-path (concat ts-emacs-dir "/github.com/dash.el"))
 (eval-after-load 'dash '(dash-enable-font-lock))
@@ -67,20 +71,10 @@
 (google-this-mode 1)
 (global-set-key (kbd "C-c / .") 'browse-url)
 
-(when (memq window-system '(mac ns x))
-  (load-file (concat ts-emacs-dir "/github.com/exec-path-from-shell/exec-path-from-shell.el"))
-  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "KUBECONFIG"))
-  (exec-path-from-shell-initialize))
-
 (setq-default buffer-file-coding-system 'iso-8859-1-unix
               c-basic-offset 2
               js-indent-level 2
               indent-tabs-mode nil)
-
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq save-abbrevs 'silently)
 
 (defvar user-temporary-file-directory
   (expand-file-name (concat user-emacs-directory "/" "autosaves" "/")))
@@ -105,10 +99,6 @@
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 (global-set-key (kbd "<home>") 'beginning-of-buffer)
 (global-set-key (kbd "<end>") 'end-of-buffer)
-(global-set-key (kbd "S-C-n") (lambda ()
-                                (interactive)
-                                (let ((default-directory "~"))
-                                  (shell (generate-new-buffer-name "*shell*")))))
 
 (setq visible-bell t)
 
@@ -164,95 +154,7 @@
 (setq mac-option-modifier nil
       mac-command-modifier 'meta)
 
-(require 'shell)
-(define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
-(define-key shell-mode-map (kbd "C-n") 'comint-next-input)
-
-(require 'tramp)
-(setq tramp-default-method "ssh")
-(setq explicit-shell-file-name "sh")
-(setq explicit-sh-args '("-l"))
-(dolist (domain local-domains)
-  (add-to-list 'tramp-default-proxies-alist
-               (list (regexp-quote domain) "\\`root\\'" "/ssh:%h:")))
-
-(defvar user-remote-shell-history-directory
-  (expand-file-name (concat user-emacs-directory "/" "shell-history" "/")))
-(make-directory user-remote-shell-history-directory t)
-
-(defun tramp-shell (method host)
-  (interactive "sMethod: \nsHost: ")
-  (find-file (format "/%s:%s:" method host))
-  (let ((buffer (current-buffer)))
-    (shell (concat method "-" host))
-    (setq comint-input-ring-file-name (concat user-remote-shell-history-directory "/" host "." method))
-    (comint-read-input-ring 'silent)
-    (kill-buffer buffer)))
-
-(defun ssh-shell (host)
-  (interactive "sHost: ")
-  (tramp-shell "ssh" host))
-
-(defun sudo-shell (host)
-  (interactive "sHost: ")
-  (tramp-shell "sudo" host))
-
-(load-file (concat ts-emacs-dir "/github.com/docker-tramp.el/docker-tramp.el"))
-(defun docker-shell (container)
-  (interactive
-   (list
-    (completing-read "Container: " (docker-tramp--running-containers))))
-  (tramp-shell "docker" container))
-
-(let ((tramp-remote-shell-executable "sh"))
-  (load-file (concat ts-emacs-dir "/github.com/kubernetes-tramp/kubernetes-tramp.el")))
-(defun pod-shell (pod)
-  (interactive
-   (list
-    (completing-read "Pod: " (kubernetes-tramp--running-containers))))
-  (tramp-shell "kubectl" pod))
-
-(add-hook 'comint-exec-hook
-          (lambda ()
-            (set-process-sentinel (get-buffer-process (current-buffer))
-                                  'shell-process-kill-buffer-sentinel)))
-
-(defun shell-process-kill-buffer-sentinel (process state)
-  (message "shell(%s): %s" (buffer-name) state)
-  (if (string-match "finished" state)
-      (kill-buffer (current-buffer))))
-
-(add-hook 'kill-buffer-hook
-          (lambda ()
-            (when (derived-mode-p 'comint-mode)
-              (comint-write-input-ring))))
-
-(add-hook 'kill-emacs-hook
-          (lambda ()
-            (loop for buffer in (buffer-list)
-                  do (progn
-                       (set-buffer buffer)
-                       (when (derived-mode-p 'comint-mode)
-                         (comint-write-input-ring))))))
-
-
-(add-to-list 'load-path (concat ts-emacs-dir "/github.com/emacs-bash-completion"))
-(require 'bash-completion)
-(bash-completion-setup)
-
 (require 'influx)
-
-(setq shell-font-lock-keywords nil)
-(setq comint-input-ring-size 50000)
-(setq comint-buffer-maximum-size 100000)
-(add-hook 'comint-output-filter-functions 'comint-truncate-buffer)
-(add-hook 'comint-mode-hook
-          '(lambda () (setq comint-input-ignoredups t)))
-
-(setq comint-password-prompt-regexp
-      (concat comint-password-prompt-regexp
-              "\\|^Password (will be hidden):\\s *\\'"
-              "\\|^Password for [^:]+:\\s*\\'"))
 
 (add-to-list 'load-path (concat ts-emacs-dir "/github.com/emacs-kubectx-mode-line"))
 (require 'kubectx)
@@ -266,7 +168,6 @@
 (global-set-key (kbd "C-c r") 'winner-redo)
 (winner-mode 1)
 
-(setq ido-ignore-buffers '("\\` " "\\`\\*tramp" "Completions\\*\\'"))
 (ido-mode 'buffers)
 
 (server-start)
---
+++ emacs/ts-shell.el
@@ -0,0 +1,102 @@
+(defvar local-domains nil "List of local domains")
+
+(defvar user-remote-shell-history-directory
+  (expand-file-name (concat user-emacs-directory "/" "shell-history" "/")))
+(make-directory user-remote-shell-history-directory t)
+
+(when (memq window-system '(mac ns x))
+  (load-file (concat ts-emacs-dir "/github.com/exec-path-from-shell/exec-path-from-shell.el"))
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "KUBECONFIG"))
+  (exec-path-from-shell-initialize))
+
+(require 'tramp)
+(setq tramp-default-method "ssh")
+(setq explicit-shell-file-name "sh")
+(setq explicit-sh-args '("-l"))
+(dolist (domain local-domains)
+  (add-to-list 'tramp-default-proxies-alist
+               (list (regexp-quote domain) "\\`root\\'" "/ssh:%h:")))
+
+(defun tramp-shell (method host)
+  (interactive "sMethod: \nsHost: ")
+  (find-file (format "/%s:%s:" method host))
+  (let ((buffer (current-buffer)))
+    (shell (concat method "-" host))
+    (setq comint-input-ring-file-name (concat user-remote-shell-history-directory "/" host "." method))
+    (comint-read-input-ring 'silent)
+    (kill-buffer buffer)))
+
+(defun ssh-shell (host)
+  (interactive "sHost: ")
+  (tramp-shell "ssh" host))
+
+(defun sudo-shell (host)
+  (interactive "sHost: ")
+  (tramp-shell "sudo" host))
+
+(load-file (concat ts-emacs-dir "/github.com/docker-tramp.el/docker-tramp.el"))
+(defun docker-shell (container)
+  (interactive
+   (list
+    (completing-read "Container: " (docker-tramp--running-containers))))
+  (tramp-shell "docker" container))
+
+(let ((tramp-remote-shell-executable "sh"))
+  (load-file (concat ts-emacs-dir "/github.com/kubernetes-tramp/kubernetes-tramp.el")))
+(defun pod-shell (pod)
+  (interactive
+   (list
+    (completing-read "Pod: " (kubernetes-tramp--running-containers))))
+  (tramp-shell "kubectl" pod))
+
+(add-hook 'comint-exec-hook
+          (lambda ()
+            (set-process-sentinel (get-buffer-process (current-buffer))
+                                  'shell-process-kill-buffer-sentinel)))
+
+(defun shell-process-kill-buffer-sentinel (process state)
+  (message "shell(%s): %s" (buffer-name) state)
+  (if (string-match "finished" state)
+      (kill-buffer (current-buffer))))
+
+(add-hook 'kill-buffer-hook
+          (lambda ()
+            (when (derived-mode-p 'comint-mode)
+              (comint-write-input-ring))))
+
+(add-hook 'kill-emacs-hook
+          (lambda ()
+            (loop for buffer in (buffer-list)
+                  do (progn
+                       (set-buffer buffer)
+                       (when (derived-mode-p 'comint-mode)
+                         (comint-write-input-ring))))))
+
+(global-set-key (kbd "S-C-n") (lambda ()
+                                (interactive)
+                                (let ((default-directory "~"))
+                                  (shell (generate-new-buffer-name "*shell*")))))
+
+(require 'shell)
+(define-key shell-mode-map (kbd "C-p") 'comint-previous-input)
+(define-key shell-mode-map (kbd "C-n") 'comint-next-input)
+
+(add-to-list 'load-path (concat ts-emacs-dir "/github.com/emacs-bash-completion"))
+(require 'bash-completion)
+(bash-completion-setup)
+
+(setq shell-font-lock-keywords nil)
+(setq comint-input-ring-size 50000)
+(setq comint-buffer-maximum-size 100000)
+(add-hook 'comint-output-filter-functions 'comint-truncate-buffer)
+(add-hook 'comint-mode-hook
+          '(lambda () (setq comint-input-ignoredups t)))
+
+(setq comint-password-prompt-regexp
+      (concat comint-password-prompt-regexp
+              "\\|^Password (will be hidden):\\s *\\'"
+              "\\|^Password for [^:]+:\\s*\\'"))
+
+(setq ido-ignore-buffers '("\\` " "\\`\\*tramp" "Completions\\*\\'"))
+
+(provide 'ts-shell)
