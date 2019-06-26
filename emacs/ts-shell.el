9295cb8a7ec3c88da5a53d43916ff90cfd2f7763
Terje Sannum <terje.sannum@nav.no>
2019-06-26 07:23:36 +0200

    use pod owner as history filename

--- emacs/ts-shell.el
+++ emacs/ts-shell.el
@@ -46,11 +46,18 @@
 
 (let ((tramp-remote-shell-executable "sh"))
   (load-file (concat ts-emacs-dir "/github.com/kubernetes-tramp/kubernetes-tramp.el")))
+
+(defun pod-owner-name (pod)
+  (let ((owner (car (apply #'process-lines kubernetes-tramp-kubectl-executable (list "get" "pod" pod "-o" "jsonpath={.metadata.ownerReferences[].kind}")))))
+    (cond ((string= owner "ReplicaSet") (replace-regexp-in-string "-[0-9a-f]\\{10\\}-[0-9a-z]\\{5\\}$" "" pod))
+          ((string= owner "DaemonSet") (replace-regexp-in-string "-[0-9a-z]\\{5\\}$" "" pod))
+          (t pod))))
+
 (defun pod-shell (pod)
   (interactive
    (list
     (completing-read "Pod: " (kubernetes-tramp--running-containers))))
-  (tramp-shell "kubectl" pod))
+  (tramp-shell "kubectl" pod (pod-owner-name pod)))
 
 (add-hook 'comint-exec-hook
           (lambda ()
