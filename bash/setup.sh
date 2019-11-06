2b93f23e20a64f8cffd2442f83185998d274edd9
Terje Sannum <terje.sannum@nav.no>
2019-11-06 07:56:31 +0100

    split bashrc

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-INSTALL_DIR=$HOME
 DIR=`cd $(dirname $0); pwd -P`
 . $DIR/../lib/functions.sh
 
-cp $DIR/profile $HOME/.profile
-cp $DIR/bash_profile $HOME/.bash_profile
-cp $DIR/bashrc $HOME/.bashrc
-cp $DIR/inputrc $HOME/.inputrc
+ln -fs $DIR/profile $HOME/.profile
+ln -fs $DIR/bash_profile $HOME/.bash_profile
+ln -fs $DIR/inputrc $HOME/.inputrc
+ln -fs $DIR/bashrc $HOME/.bashrc
+ln -fs $DIR/bashrc.emacshost $HOME/.bashrc.emacshost
 
-github_clone_or_pull $INSTALL_DIR/lib/bash/github.com \
+github_clone_or_pull $HOME/lib/bash/github.com \
                      https://github.com/cykerway/complete-alias.git \
                      https://github.com/ahmetb/kubectx.git \
                      https://github.com/politza/ealias.git
 
 mkdir -p $HOME/bin
-ln -fs $INSTALL_DIR/lib/bash/github.com/kubectx/kubectx $HOME/bin/kubectx
-ln -fs $INSTALL_DIR/lib/bash/github.com/kubectx/kubens $HOME/bin/kubens
+ln -fs $HOME/lib/bash/github.com/kubectx/kubectx $HOME/bin/kubectx
+ln -fs $HOME/lib/bash/github.com/kubectx/kubens $HOME/bin/kubens
