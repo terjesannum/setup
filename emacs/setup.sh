56d92d95901c67421a9f7d36bbcc1eb582cb4131
Terje Sannum <terje.sannum@nav.no>
2023-08-01 15:13:36 +0200

    clone emacs-bash-completion

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -6,11 +6,11 @@ DIR=`cd $(dirname $0); pwd -P`
 mkdir -p $HOME/.emacs.d/github.com
 ln -fs $DIR/init.el $HOME/.emacs.d/init.el
 
-github_clone_or_pull $HOME/.emacs.d/github.com \
-    https://github.com/terjesannum/cyberpunk-theme.el.git \
-    https://github.com/terjesannum/org-mac-iCal.git \
-    https://github.com/terjesannum/emacs-gcloud-mode.git \
-    https://github.com/terjesannum/emacs-kubectx-mode.git \
-    https://github.com/terjesannum/emacs-influxdb-mode.git \
-    https://github.com/terjesannum/emacs-shell.git \
-    https://github.com/jwiegley/use-package.git
+github_clone_or_pull https://github.com/terjesannum/cyberpunk-theme.el.git
+github_clone_or_pull https://github.com/terjesannum/org-mac-iCal.git
+github_clone_or_pull https://github.com/terjesannum/emacs-gcloud-mode.git
+github_clone_or_pull https://github.com/terjesannum/emacs-kubectx-mode.git
+github_clone_or_pull https://github.com/terjesannum/emacs-influxdb-mode.git
+github_clone_or_pull https://github.com/terjesannum/emacs-shell.git
+github_clone_or_pull https://github.com/jwiegley/use-package.git
+github_clone_or_pull https://github.com/szermatt/emacs-bash-completion.git 8e9c20dbfe01d8bf6c61db231593623a201c75c6
--- lib/functions.sh
+++ lib/functions.sh
@@ -1,13 +1,17 @@
 function github_clone_or_pull {
-    dir=$1
+    repo=$1
+    commit=$2
+    dir=$HOME/.emacs.d/github.com
     mkdir -p $dir
-    shift
-    for repo in $*; do
-        repodir=$dir/$(basename $repo .git)
-        if test -d $repodir; then
+    repodir=$dir/$(basename $repo .git)
+    if test -d $repodir; then
+        if test -z "$commit"; then
             (cd $repodir; git pull)
-        else
-            (cd $dir; git clone $repo)
         fi
-    done
+    else
+        (cd $dir;
+         git clone $repo;
+         if test -n "$commit"; then cd $repodir && git checkout $commit; fi
+        )
+    fi
 }
