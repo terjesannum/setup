6415cfc224d1c3a2980beb94296e3afea4d26e70
Terje Sannum <terje.sannum@nav.no>
2026-02-05 11:03:23 +0100

    use gpt-5.2

--- emacs/init.el
+++ emacs/init.el
@@ -231,7 +231,7 @@
   :hook (prog-mode . copilot-mode))
 (use-package gptel
   :config (setq gptel-backend (gptel-make-gh-copilot "Copilot")
-                gptel-model 'gpt-5)
+                gptel-model 'gpt-5.2)
   :hook (gptel-post-response . gptel-end-of-response))
 
 (when (eq system-type 'darwin)
