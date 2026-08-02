5e177404986ce134a5a1957415915c59244914e3
Terje Sannum <terje.sannum@nav.no>
2026-08-02 12:22:44 +0200

    only add allow-url arguments if whitelist exists

--- emacs/init.el
+++ emacs/init.el
@@ -234,15 +234,17 @@
   :config
   (setq agent-shell-dot-subdir-function #'ts-agent-shell-dir
         agent-shell-preferred-agent-config (agent-shell-github-make-copilot-config)
-        agent-shell-github-acp-command (append '("cplt" "-q" "-y" "--" "--acp")
-                                               (mapcan (lambda (url) (list "--allow-url" url))
-                                                       (with-temp-buffer
-                                                         (insert-file-contents (expand-file-name "~/.config/cplt/allowed-domains.txt"))
-                                                         (split-string (buffer-string) "\n" t)))
-                                               (mapcan (lambda (tool) (list "--allow-tool" tool))
-                                                       ts-agent-shell-allowed-tools)
-                                               (mapcan (lambda (dir) (list "--add-dir" dir))
-                                                       ts-agent-shell-allowed-dirs))
+        agent-shell-github-acp-command (let ((url-whitelist (expand-file-name "~/.config/cplt/allowed-domains.txt")))
+                                         (append '("cplt" "-q" "-y" "--" "--acp")
+                                                 (when (file-exists-p url-whitelist)
+                                                   (mapcan (lambda (url) (list "--allow-url" url))
+                                                           (with-temp-buffer
+                                                             (insert-file-contents url-whitelist)
+                                                             (split-string (buffer-string) "\n" t))))
+                                                 (mapcan (lambda (tool) (list "--allow-tool" tool))
+                                                         ts-agent-shell-allowed-tools)
+                                                 (mapcan (lambda (dir) (list "--add-dir" dir))
+                                                         ts-agent-shell-allowed-dirs)))
         agent-shell-github-default-model-id "claude-opus-5"
         agent-shell-context-sources '(files))
   (advice-add 'agent-shell--ensure-gitignore :override #'ignore)
